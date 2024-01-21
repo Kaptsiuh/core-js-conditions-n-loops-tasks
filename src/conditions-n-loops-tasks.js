3879e582773f9e15b132f4d249b7cf26ea75a4b8
Dima Berry <dimaberry1@gmail.com>
2024-01-21 20:04:29 +0100

    feat: solve sorts an array of number in ascending order in place

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -398,8 +398,19 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const cloneArr = arr;
+  let currentElement = [];
+  for (let i = 1; i < cloneArr.length; i += 1) {
+    currentElement = cloneArr[i];
+    let j = i - 1;
+    for (; j >= 0 && cloneArr[j] > currentElement; ) {
+      cloneArr[j + 1] = cloneArr[j];
+      j -= 1;
+    }
+    cloneArr[j + 1] = currentElement;
+  }
+  return cloneArr;
 }
 
 /**
